82b481a7d0b8bef3e74ed2b277b6e53e3cb801d0
will <lawsonw2@gmail.com>
2020-02-24 20:51:05 -0500

    Updates

---
+++ precache-manifest.92935b8e81b9c0acffd32fe491efad7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da0b578f032ae15de45643a5665343f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "508db8de4d23df684b8a",
+    "url": "/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "42f9f4cb72be36e40dae",
+    "url": "/static/css/main.bc4271d6.chunk.css"
+  },
+  {
+    "revision": "508db8de4d23df684b8a",
+    "url": "/static/js/2.65a44816.chunk.js"
+  },
+  {
+    "revision": "42f9f4cb72be36e40dae",
+    "url": "/static/js/main.1b275856.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
